1b1423fa9f1f7f03edbc76b5bcd934989bcda44f
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2023-08-01 15:52:03 +0800

    1.0.1 - Added toCommaSeparatedString

--- src/rounding.ts
+++ src/rounding.ts
@@ -7,3 +7,4 @@ export const BASE_CURRENCY_DP = 2
 export const toUnitsString = (value: Big): string => value.toFixed(UNITS_DP, Big.roundHalfEven)
 export const toUnitPriceString = (value: Big): string => value.toFixed(UNIT_PRICE_DP, Big.roundHalfEven)
 export const toBaseCurrencyString = (value: Big): string => value.toFixed(BASE_CURRENCY_DP, Big.roundHalfEven)
+export const toCommaSeparatedString = (value: Big, dp: number) => Number(value.toFixed(dp, Big.roundHalfEven)).toLocaleString(undefined, { minimumFractionDigits: dp, maximumFractionDigits: dp })
