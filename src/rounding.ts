b27dbffea95d4aea91ba69a5b611ed1176a659b6
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2023-08-02 12:53:45 +0800

    1.1.0 - Modified number formatting functions

--- src/rounding.ts
+++ src/rounding.ts
@@ -4,7 +4,33 @@ export const UNITS_DP = 4
 export const UNIT_PRICE_DP = 4
 export const BASE_CURRENCY_DP = 2
 
-export const toUnitsString = (value: Big): string => value.toFixed(UNITS_DP, Big.roundHalfEven)
-export const toUnitPriceString = (value: Big): string => value.toFixed(UNIT_PRICE_DP, Big.roundHalfEven)
-export const toBaseCurrencyString = (value: Big): string => value.toFixed(BASE_CURRENCY_DP, Big.roundHalfEven)
-export const toCommaSeparatedString = (value: Big, dp: number) => Number(value.toFixed(dp, Big.roundHalfEven)).toLocaleString(undefined, { minimumFractionDigits: dp, maximumFractionDigits: dp })
+export const THOUSANDS_SEPARATOR = ","
+export const DECIMAL_SEPARATOR = "."
+
+export const BASE_CURRENCY_SYMBOL = "$"
+export const BASE_CURRENCY_CODE = "AUD"
+
+function format(value: Big, thousandsSeparator: string, decimalSeparator: string, decimalPlaces: number) {
+    var arr = value.toFixed(decimalPlaces, Big.roundHalfEven).split(".")
+    arr[0] = arr[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousandsSeparator == null ? "," : thousandsSeparator + "")
+    return arr.join(decimalSeparator == null ? "." : decimalSeparator + "")
+}
+
+interface FormattingOptions {
+    commaSeparated?: boolean
+}
+
+type CurrencyMark = "Symbol" | "Code"
+
+interface CurrencyFormattingOptions extends FormattingOptions {
+    prependCurrencyMark?: CurrencyMark
+}
+
+const getPrefix = (currencyMark: CurrencyMark | undefined): string => currencyMark === "Symbol" ? BASE_CURRENCY_SYMBOL : currencyMark === "Code" ? BASE_CURRENCY_CODE + " " : ""
+
+export const toUnitsString = (value: Big, options?: FormattingOptions): string =>
+    format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, UNITS_DP)
+export const toUnitPriceString = (value: Big, options?: CurrencyFormattingOptions): string =>
+    getPrefix(options?.prependCurrencyMark) + format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, UNIT_PRICE_DP)
+export const toBaseCurrencyString = (value: Big, options?: CurrencyFormattingOptions): string =>
+    getPrefix(options?.prependCurrencyMark) + format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, BASE_CURRENCY_DP)
--- test/rounding.test.ts
+++ test/rounding.test.ts
@@ -2,27 +2,86 @@ import { Big } from "big.js"
 import { toUnitsString, toUnitPriceString, toBaseCurrencyString } from "../src/rounding"
 
 const val1 = new Big("1.55555")
-const val2 = new Big("1.56565")
-const val3 = new Big("1.555")
-const val4 = new Big("1.565")
+const val2 = new Big("100.56565")
+const val3 = new Big("1000.555")
+const val4 = new Big("10000.565")
 
-test("to units string", () => {
-    expect(toUnitsString(val1)).toEqual("1.5556")
-    expect(toUnitsString(val2)).toEqual("1.5656")
-    expect(toUnitsString(val3)).toEqual("1.5550")
-    expect(toUnitsString(val4)).toEqual("1.5650")
+const commaSeparatedOptions = { commaSeparated: true }
+const currencySymbolOptions: { prependCurrencyMark: "Symbol" } = { prependCurrencyMark: "Symbol" }
+const currencyCodeOptions: { prependCurrencyMark: "Code" } = { prependCurrencyMark: "Code" }
+
+describe("to units string", () => {
+    test("rounding", () => {
+        expect(toUnitsString(val1)).toEqual("1.5556")
+        expect(toUnitsString(val2)).toEqual("100.5656")
+        expect(toUnitsString(val3)).toEqual("1000.5550")
+        expect(toUnitsString(val4)).toEqual("10000.5650")
+    })
+
+    test("comma separating", () => {
+        expect(toUnitsString(val1, commaSeparatedOptions)).toEqual("1.5556")
+        expect(toUnitsString(val2, commaSeparatedOptions)).toEqual("100.5656")
+        expect(toUnitsString(val3, commaSeparatedOptions)).toEqual("1,000.5550")
+        expect(toUnitsString(val4, commaSeparatedOptions)).toEqual("10,000.5650")
+    })
 })
 
-test("to unit price string", () => {
-    expect(toUnitPriceString(val1)).toEqual("1.5556")
-    expect(toUnitPriceString(val2)).toEqual("1.5656")
-    expect(toUnitPriceString(val3)).toEqual("1.5550")
-    expect(toUnitPriceString(val4)).toEqual("1.5650")
+describe("to unit price string", () => {
+    test("rounding", () => {
+        expect(toUnitPriceString(val1)).toEqual("1.5556")
+        expect(toUnitPriceString(val2)).toEqual("100.5656")
+        expect(toUnitPriceString(val3)).toEqual("1000.5550")
+        expect(toUnitPriceString(val4)).toEqual("10000.5650")
+    })
+
+    test("comma separating", () => {
+        expect(toUnitPriceString(val1, commaSeparatedOptions)).toEqual("1.5556")
+        expect(toUnitPriceString(val2, commaSeparatedOptions)).toEqual("100.5656")
+        expect(toUnitPriceString(val3, commaSeparatedOptions)).toEqual("1,000.5550")
+        expect(toUnitPriceString(val4, commaSeparatedOptions)).toEqual("10,000.5650")
+    })
+
+    test("currency symbol", () => {
+        expect(toUnitPriceString(val1, currencySymbolOptions)).toEqual("$1.5556")
+        expect(toUnitPriceString(val2, currencySymbolOptions)).toEqual("$100.5656")
+        expect(toUnitPriceString(val3, currencySymbolOptions)).toEqual("$1000.5550")
+        expect(toUnitPriceString(val4, currencySymbolOptions)).toEqual("$10000.5650")
+    })
+
+    test("currency code", () => {
+        expect(toUnitPriceString(val1, currencyCodeOptions)).toEqual("AUD 1.5556")
+        expect(toUnitPriceString(val2, currencyCodeOptions)).toEqual("AUD 100.5656")
+        expect(toUnitPriceString(val3, currencyCodeOptions)).toEqual("AUD 1000.5550")
+        expect(toUnitPriceString(val4, currencyCodeOptions)).toEqual("AUD 10000.5650")
+    })
 })
 
-test("to base currency string", () => {
-    expect(toBaseCurrencyString(val1)).toEqual("1.56")
-    expect(toBaseCurrencyString(val2)).toEqual("1.57")
-    expect(toBaseCurrencyString(val3)).toEqual("1.56")
-    expect(toBaseCurrencyString(val4)).toEqual("1.56")
+describe("to base currency string", () => {
+    test("rounding", () => {
+        expect(toBaseCurrencyString(val1)).toEqual("1.56")
+        expect(toBaseCurrencyString(val2)).toEqual("100.57")
+        expect(toBaseCurrencyString(val3)).toEqual("1000.56")
+        expect(toBaseCurrencyString(val4)).toEqual("10000.56")
+    })
+
+    test("comma separating", () => {
+        expect(toBaseCurrencyString(val1, commaSeparatedOptions)).toEqual("1.56")
+        expect(toBaseCurrencyString(val2, commaSeparatedOptions)).toEqual("100.57")
+        expect(toBaseCurrencyString(val3, commaSeparatedOptions)).toEqual("1,000.56")
+        expect(toBaseCurrencyString(val4, commaSeparatedOptions)).toEqual("10,000.56")
+    })
+
+    test("currency symbol", () => {
+        expect(toBaseCurrencyString(val1, currencySymbolOptions)).toEqual("$1.56")
+        expect(toBaseCurrencyString(val2, currencySymbolOptions)).toEqual("$100.57")
+        expect(toBaseCurrencyString(val3, currencySymbolOptions)).toEqual("$1000.56")
+        expect(toBaseCurrencyString(val4, currencySymbolOptions)).toEqual("$10000.56")
+    })
+
+    test("currency code", () => {
+        expect(toBaseCurrencyString(val1, currencyCodeOptions)).toEqual("AUD 1.56")
+        expect(toBaseCurrencyString(val2, currencyCodeOptions)).toEqual("AUD 100.57")
+        expect(toBaseCurrencyString(val3, currencyCodeOptions)).toEqual("AUD 1000.56")
+        expect(toBaseCurrencyString(val4, currencyCodeOptions)).toEqual("AUD 10000.56")
+    })
 })
