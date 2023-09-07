e8c1a77bfd1d9138b011d2d19ea079b5ef1ad8da
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2023-09-07 13:07:39 +0800

    Improved negative number formatting

--- src/formatting.ts
+++ src/formatting.ts
@@ -10,14 +10,16 @@ export const DECIMAL_SEPARATOR = "."
 export const BASE_CURRENCY_SYMBOL = "$"
 export const BASE_CURRENCY_CODE = "AUD"
 
-function format(value: Big, thousandsSeparator: string, decimalSeparator: string, decimalPlaces: number) {
-    var arr = value.toFixed(decimalPlaces, Big.roundHalfEven).split(".")
+function format(value: Big, thousandsSeparator: string, decimalSeparator: string, decimalPlaces: number, prefix?: string, bracketsForNegatives?: boolean) {
+    var arr = value.abs().toFixed(decimalPlaces, Big.roundHalfEven).split(".")
     arr[0] = arr[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousandsSeparator == null ? "," : thousandsSeparator + "")
-    return arr.join(decimalSeparator == null ? "." : decimalSeparator + "")
+    const str = prefix + arr.join(decimalSeparator == null ? "." : decimalSeparator + "")
+    return value.lt(0) ? bracketsForNegatives ? `(${str})` : `-${str}` : str
 }
 
 interface FormattingOptions {
-    commaSeparated?: boolean
+    commaSeparated?: boolean,
+    bracketsForNegatives?: boolean
 }
 
 type CurrencyMark = "Symbol" | "Code"
@@ -29,8 +31,8 @@ interface CurrencyFormattingOptions extends FormattingOptions {
 const getPrefix = (currencyMark: CurrencyMark | undefined): string => currencyMark === "Symbol" ? BASE_CURRENCY_SYMBOL : currencyMark === "Code" ? BASE_CURRENCY_CODE + " " : ""
 
 export const toUnitsString = (value: Big, options?: FormattingOptions): string =>
-    format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, UNITS_DP)
+    format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, UNITS_DP, "", options?.bracketsForNegatives)
 export const toUnitPriceString = (value: Big, options?: CurrencyFormattingOptions): string =>
-    getPrefix(options?.prependCurrencyMark) + format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, UNIT_PRICE_DP)
+    format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, UNIT_PRICE_DP, getPrefix(options?.prependCurrencyMark), options?.bracketsForNegatives)
 export const toBaseCurrencyString = (value: Big, options?: CurrencyFormattingOptions): string =>
-    getPrefix(options?.prependCurrencyMark) + format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, BASE_CURRENCY_DP)
+    format(value, options?.commaSeparated ? THOUSANDS_SEPARATOR : "", DECIMAL_SEPARATOR, BASE_CURRENCY_DP, getPrefix(options?.prependCurrencyMark), options?.bracketsForNegatives)
